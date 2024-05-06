964aed4639af1a1aaefd2cfc599cff095935b3a1
VikashY-ITL <vikas.y@intutechlab.com>
2024-05-06 11:38:51 +0530

    started

---
+++ README.md
@@ -0,0 +1,3 @@
+# React + Vite
+
+This is web developer`s portfolio
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './app.scss'
+
+const App = () => {
+  return <div>
+    <section> <a href="#">Home</a> </section>
+    <section>portpolio</section>
+    <section>portpolio2</section>
+    <section>portpolio3</section>
+    <section>portpolio4</section>
+    <section>portpolio5</section>
+  </div>;
+};
+
+export default App;
